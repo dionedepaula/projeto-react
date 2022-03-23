4584bc66b6c033a92acc600daf566a37b2a87f29
Dione de Paula de Sousa Oliveira <dione3967@gmail.com>
2022-03-22 22:23:55 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Menu from "./components/Menu";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Card, ListGroup } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Menu />
+      <h1>Hello World</h1>
+
+      <Button variant="primary">Primary</Button>
+
+      <Card border="secondary" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Secondary Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
     </div>
   );
 }
